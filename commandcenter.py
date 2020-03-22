28a00bb2c2999cb6f1266015df0e621cb2ca4af2
Giridhar Kaushik <giridharkaushikr@gmail.com>
2020-03-22 18:15:34 -0400

    outline1

---
+++ commandcenter.py
@@ -0,0 +1,157 @@
+'''
+commandcenter.py
+###########################
+The idea is to look at this at any point of time and 
+get a snapshot of your life situation and answer two questions. 
+1. How are you ?
+2. How is everything ?
+##########################
+How are you ?
+	Physically
+	Mentally
+How is everything ?
+	Your home
+		Is everything in its place ?
+	Your Finances
+		Do you have enough money to manage
+			Food
+			Medication
+			Fees
+	Your Groceries supply
+##################################
+Bath
+	Floor
+	toilet bowl
+	Washbasin
+	countertop
+	Bath floor
+	Bath glass door
+
+Kitchen
+	Dishes
+	Garbage
+		Trash
+			full == T ?
+				throw trash
+				change cover
+			empty
+		Recycling
+	dishwasher
+		if on
+			timer
+		if off
+		dishes clean ?
+			unload
+		dishes dirty?
+			load more
+		n(containers in sink) > 5
+			load
+	Floor
+
+Clothes
+	no of shirts =
+	no of undies =
+	no of vests=
+	no of jeans =
+
+
+Daily finance
+	Income
+		Biweekly
+	Expense
+		Daily- ability to add an expense
+		Weekly
+		Monthly
+			Shared
+				Rent
+				Utilities
+				Wifi
+				Electricity
+			Individual
+				Subscriptions
+					Netflix
+					Amazon prime
+
+Water
+	Measure drinking water drunk per day
+	if No of gallons left < 2 gallon
+		add to grocery list
+	else
+		pass
+
+Food
+	Breakfast
+		Options
+			Add food
+		Time
+	Lunch
+		Options
+			Add food
+		Time
+	Dinner
+		Options
+			Add food
+		Time
+	snack
+		Options
+			Add food
+		Time
+
+Medication
+	Add pill
+		Frequency
+		Time
+		Dosage
+
+Self
+	Hair
+		Haircut
+		Beard cut
+		Body hair
+	Nails
+		Fingers
+		Toes
+	Teeth
+		Floss
+		Deep clean
+	Body
+		Weight
+		Exercise
+			Exercise schedule
+				M
+				T
+				W
+				R
+				F
+				S
+				S
+
+Devices
+	Mobile phone
+		Battery level
+		Last cleaned
+		Last disinfected
+	Laptop
+		Last cleaned
+		Last disinfected
+	Desktop
+		Last cleaned
+		Last disinfected
+
+Room
+	Bed
+		Bedsheet
+			Last cleaned
+			change
+		Pillow
+			Last cleaned
+			change
+		Mattress protector
+			Last cleaned
+			change
+
+
+
+
+
+
